9a234da33cf7387d3ab9e3dfde13b2e045f3666f
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-11-07 14:11:54 +0100

    update

--- app/src/main/java/es/icjardin/mapaciudadesalesandro/MapsActivity.kt
+++ app/src/main/java/es/icjardin/mapaciudadesalesandro/MapsActivity.kt
@@ -32,39 +32,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var db: PointDatabaseHelper
 
     /**
-     * Lista de ciudades
-     */
-    val ciudades: List<Point> = listOf(
-        Point(0, "Vitoria-Gasteiz", "Capital del País Vasco", 42.849998, -2.683333,"vitoria","Capital del País Vasco, conocida por su enfoque en la sostenibilidad y la calidad de vida. Destaca por su casco medieval bien conservado y el anillo verde que rodea la ciudad, ideal para el senderismo y el ciclismo."),
-        Point(0, "Bilbao", "Centro económico y cultural del País Vasco", 43.263056, -2.934611,"bilbao","Centro económico y cultural del País Vasco, famosa por el Museo Guggenheim, que impulsó su transformación urbana. Es una ciudad moderna y dinámica, con una rica gastronomía y tradiciones vascas."),
-        Point(0, "Madrid", "Capital de España", 40.416775, -3.703790,"madrid","Capital de España y una de las ciudades más vibrantes de Europa. Alberga una mezcla de historia y modernidad, con monumentos emblemáticos como el Palacio Real y el Museo del Prado, junto con una vida nocturna animada y diversa."),
-        Point(0, "Barcelona", "Capital de Cataluña", 41.385064, 2.173403,"barcelona","Capital de Cataluña, reconocida por su arquitectura modernista, especialmente las obras de Antoni Gaudí como la Sagrada Familia. Tiene un clima mediterráneo, playas y una vida cultural activa."),
-        Point(0, "Sevilla", "Capital de Andalucia", 37.382830, -5.973174,"sevilla","Capital de Andalucía, famosa por su flamenco, su rica historia y monumentos icónicos como la Giralda y la Plaza de España. Sus calles estrechas y su gastronomía la convierten en un destino encantador."),
-        Point(0, "Valencia", "Ciudad costera conocida por su Ciudad de las Artes y las Ciencias", 39.466667, -0.375000,"valencia","Ciudad costera conocida por su Ciudad de las Artes y las Ciencias, un impresionante complejo arquitectónico. También es famosa por la paella y por albergar las tradicionales Fallas, una colorida festividad anual."),
-        Point(0, "Zaragoza", "Capital de Aragón", 41.650002, -0.883, "zaragoza","Capital de Aragón, famosa por la Basílica del Pilar a orillas del río Ebro. Ofrece una mezcla de historia romana, musulmana y cristiana, y destaca por su gastronomía y su ambiente acogedor."),
-        Point(0, "Valladolid", "Ciudad con un gran patrimonio histórico", 41.6544, -4.723611, "valladolid","Ciudad con un gran patrimonio histórico, famosa por ser un importante centro en la época de los Reyes Católicos. Sus monumentos, como la Catedral y la Plaza Mayor, reflejan su importancia histórica y cultural."),
-        Point(0, "Salamanca", "Hogar de una de las universidades más antiguas de Europa", 40.966668, -5.650000, "salamanca","Hogar de una de las universidades más antiguas de Europa, la Universidad de Salamanca. Su casco histórico es Patrimonio de la Humanidad y destaca por sus edificios renacentistas, como la Plaza Mayor y la Casa de las Conchas."),
-        Point(0, "Oviedo", "Capital de Asturias", 43.366667, -5.800000, "oviedo","Capital de Asturias, conocida por su casco antiguo medieval y la catedral de San Salvador. Es una ciudad limpia y ordenada, famosa por su sidra y por ser un punto importante en el Camino de Santiago.")
-    )
-
-    /**
      * Función que se ejecuta al crear la actividad
      *
      * @param savedInstanceState estado de la actividad
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         db = PointDatabaseHelper(this)
 
-        db.deleteAllPoints() // Elimino todos los puntos de la tabla
-
-        // Cargar las ciudades a la base de datos
-        for (ciudad in ciudades) {
-            db.insertPoint(ciudad)
-        }
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -79,6 +57,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE // Tipo del mapa
 
         // Cargar los puntos
         val points: List<Point> = db.getAllPoints() // Obtiene todos los puntos de la base de datos
--- app/src/main/java/es/icjardin/mapaciudadesalesandro/PointDatabaseHelper.kt
+++ app/src/main/java/es/icjardin/mapaciudadesalesandro/PointDatabaseHelper.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 /**
  * Clase con funciones para interactuar con la base de datos de SQLite
@@ -25,6 +26,22 @@ class PointDatabaseHelper(context: Context) :
     }
 
     /**
+     * Lista de ciudades
+     */
+    private val ciudades: List<Point> = listOf(
+        Point(0, "Vitoria-Gasteiz", "Capital del País Vasco", 42.849998, -2.683333,"vitoria","Capital del País Vasco, conocida por su enfoque en la sostenibilidad y la calidad de vida. Destaca por su casco medieval bien conservado y el anillo verde que rodea la ciudad, ideal para el senderismo y el ciclismo."),
+        Point(0, "Bilbao", "Centro económico y cultural del País Vasco", 43.263056, -2.934611,"bilbao","Centro económico y cultural del País Vasco, famosa por el Museo Guggenheim, que impulsó su transformación urbana. Es una ciudad moderna y dinámica, con una rica gastronomía y tradiciones vascas."),
+        Point(0, "Madrid", "Capital de España", 40.416775, -3.703790,"madrid","Capital de España y una de las ciudades más vibrantes de Europa. Alberga una mezcla de historia y modernidad, con monumentos emblemáticos como el Palacio Real y el Museo del Prado, junto con una vida nocturna animada y diversa."),
+        Point(0, "Barcelona", "Capital de Cataluña", 41.385064, 2.173403,"barcelona","Capital de Cataluña, reconocida por su arquitectura modernista, especialmente las obras de Antoni Gaudí como la Sagrada Familia. Tiene un clima mediterráneo, playas y una vida cultural activa."),
+        Point(0, "Sevilla", "Capital de Andalucia", 37.382830, -5.973174,"sevilla","Capital de Andalucía, famosa por su flamenco, su rica historia y monumentos icónicos como la Giralda y la Plaza de España. Sus calles estrechas y su gastronomía la convierten en un destino encantador."),
+        Point(0, "Valencia", "Ciudad costera conocida por su Ciudad de las Artes y las Ciencias", 39.466667, -0.375000,"valencia","Ciudad costera conocida por su Ciudad de las Artes y las Ciencias, un impresionante complejo arquitectónico. También es famosa por la paella y por albergar las tradicionales Fallas, una colorida festividad anual."),
+        Point(0, "Zaragoza", "Capital de Aragón", 41.650002, -0.883, "zaragoza","Capital de Aragón, famosa por la Basílica del Pilar a orillas del río Ebro. Ofrece una mezcla de historia romana, musulmana y cristiana, y destaca por su gastronomía y su ambiente acogedor."),
+        Point(0, "Valladolid", "Ciudad con un gran patrimonio histórico", 41.6544, -4.723611, "valladolid","Ciudad con un gran patrimonio histórico, famosa por ser un importante centro en la época de los Reyes Católicos. Sus monumentos, como la Catedral y la Plaza Mayor, reflejan su importancia histórica y cultural."),
+        Point(0, "Salamanca", "Hogar de una de las universidades más antiguas de Europa", 40.966668, -5.650000, "salamanca","Hogar de una de las universidades más antiguas de Europa, la Universidad de Salamanca. Su casco histórico es Patrimonio de la Humanidad y destaca por sus edificios renacentistas, como la Plaza Mayor y la Casa de las Conchas."),
+        Point(0, "Oviedo", "Capital de Asturias", 43.366667, -5.800000, "oviedo","Capital de Asturias, conocida por su casco antiguo medieval y la catedral de San Salvador. Es una ciudad limpia y ordenada, famosa por su sidra y por ser un punto importante en el Camino de Santiago.")
+    )
+
+    /**
      * Función que crea la tabla
      *
      * @param db base de datos
@@ -33,13 +50,14 @@ class PointDatabaseHelper(context: Context) :
         val createTableQuery =
             "CREATE TABLE $TABLE_NAME (" +
                     "$COLUMN_ID INTEGER PRIMARY KEY, " +
-                    "$COLUMN_TITLE TEXT, " +
+                    "$COLUMN_TITLE TEXT UNIQUE, " +
                     "$COLUMN_DESCRIPTION TEXT, " +
                     "$COLUMN_LAT DOUBLE, " +
                     "$COLUMN_LON DOUBLE, " +
                     "$COLUMN_IMAGE TEXT, " +
                     "$COLUMN_TEXT TEXT)"
         db?.execSQL(createTableQuery)
+        cargarCiudades() // Cargar ciudades
     }
 
     /**
@@ -54,6 +72,16 @@ class PointDatabaseHelper(context: Context) :
             "DROP TABLE IF EXISTS $TABLE_NAME"
         db?.execSQL(dropTableQuery)
         onCreate(db)
+    }
+
+    /**
+     * Carga las ciudades en la base de datos
+     */
+    private fun cargarCiudades() {
+        Log.d("cargarCiudades", "CARGANDO")
+        for (ciudad in ciudades) {
+            insertPoint(ciudad)
+        }
     }
 
     /**
@@ -113,33 +141,6 @@ class PointDatabaseHelper(context: Context) :
     }
 
     /**
-     * Recibe un integer que es su posición en la lista, con esto recuperara los datos del point
-     *
-     * @param idPoint id del punto
-     * @return point
-     */
-    fun getIdPoint(idPoint: Int): Point {
-        val db = readableDatabase
-        val query = "SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID=$idPoint"
-        //lanza un cursor
-        val cursor = db.rawQuery(query, null)
-        //ve al primer registro que cumpla esa condicion (esperemos que el único)
-        cursor.moveToFirst()
-        //leo los datos de la consulta
-        val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
-        val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
-        val description = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DESCRIPTION))
-        val lat = cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_LAT))
-        val lon = cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_LON))
-        val image = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_IMAGE))
-        val text = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TEXT))
-        //cierro conexiones y devuelvo la task
-        cursor.close()
-        db.close()
-        return Point(id, title, description, lat, lon, image, text)
-    }
-
-    /**
      * Recibe un string con el título del punto, y devuelve el punto correspondiente de la base de datos
      *
      * @param titlePoint título del punto
@@ -164,15 +165,6 @@ class PointDatabaseHelper(context: Context) :
         cursor.close()
         db.close()
         return Point(id, title, description, lat, lon, image, text)
-    }
-
-    /**
-     * Elimina todos los puntos de la tabla
-     */
-    fun deleteAllPoints() {
-        val db = writableDatabase
-        db.delete(TABLE_NAME, null, null)
-        db.close()
     }
 
 }
